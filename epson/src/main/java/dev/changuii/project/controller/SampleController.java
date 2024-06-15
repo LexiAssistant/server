2fc843de5223c51fd3704ff982befbb49e722e18
changuii <rhljh7410@gmail.com>
2024-06-15 22:18:03 +0900

    ✅ tesseract 완료, env 파일 작성해야함

--- epson/src/main/java/dev/changuii/project/controller/SampleController.java
+++ epson/src/main/java/dev/changuii/project/controller/SampleController.java
@@ -1,14 +1,39 @@
 package dev.changuii.project.controller;
 
 
+import dev.changuii.project.service.TesseractService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/sample")
 public class SampleController {
 
 
+    private TesseractService tesseractService;
 
+    public SampleController(
+            @Autowired TesseractService tesseractService
+    ){
+        this.tesseractService = tesseractService;
+    }
+
+
+    @PostMapping("")
+    public String imageToString(
+            @RequestPart("image")MultipartFile multipartFile
+            ) {
+        try {
+            return this.tesseractService.imageToString(multipartFile);
+        } catch (IOException e) {
+            return "error";
+        }
+    }
 
 }
